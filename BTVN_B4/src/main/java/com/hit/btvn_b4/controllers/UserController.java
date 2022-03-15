7bab699127866da3465fabe029076d4b0948e0b4
Only Huân <71635685+huannd0101@users.noreply.github.com>
2022-03-15 23:36:32 +0700

    BTVN B4

---
+++ BTVN_B4/src/main/java/com/hit/btvn_b4/controllers/UserController.java
@@ -0,0 +1,55 @@
+package com.hit.btvn_b4.controllers;
+
+import com.hit.btvn_b4.dto.UserDTO;
+import com.hit.btvn_b4.models.User;
+import com.hit.btvn_b4.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public ResponseEntity<?> getAllUser() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.status(200).body(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable(name = "id") Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return ResponseEntity.status(200).body(user.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createNewUser(@RequestBody UserDTO userDTO) {
+        User user = new User();
+        user.setUsername(userDTO.getUsername());
+        user.setPassword(userDTO.getPassword());
+        user.setFullName(userDTO.getFullName());
+
+        userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Đã tạo thành công");
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ BTVN_B4/src/test/java/com/hit/btvn_b4/BtvnB4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hit.btvn_b4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtvnB4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
