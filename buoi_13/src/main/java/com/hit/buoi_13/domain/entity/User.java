219a9f69e4b13e24b58c512cac652679a1371a33
Only Huân <71635685+huannd0101@users.noreply.github.com>
2022-06-29 22:01:42 +0700

    Buổi 2: liquibase

---
+++ buoi_13/src/main/java/com/hit/buoi_13/domain/entity/User.java
@@ -0,0 +1,16 @@
+package com.hit.buoi_13.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+public class User {
+
+
+
+}
